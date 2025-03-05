7f1d1ea303d697b355ea31973967fa2076e863e9
Monica Antao <monica.sn.antao@gmail.com>
2025-03-05 11:41:28 +0000

    App inplementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
+import { useState } from "react";
+import Header from "./components/Header.jsx";
+import Results from "./components/Results.jsx";
+import UserInput from "./components/UserInput.jsx";
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const inputIsValid = userInput.duration >= 1;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header></Header>
+      <UserInput onChange={handleChange} userInput={userInput}></UserInput>
+      {inputIsValid ? (
+        <Results input={userInput} />
+      ) : (
+        <p className="center">Please enter a duration greater than zero</p>
+      )}
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import logo from '../assets/investment-calculator-logo.png'
+
+export default function Header() {
+  return (
+    <header id='header'>
+      <img src={logo} alt='logo showing a money bag'></img>
+      <h1>Investment calculator</h1>
+    </header>
+  );
+}
